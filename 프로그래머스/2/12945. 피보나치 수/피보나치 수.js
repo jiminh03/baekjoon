776aa09718c166569e6afe351792d8a662ee10fa
Jimin Hwang <101847439+jiminh03@users.noreply.github.com>
2025-10-31 20:23:07 +0900

    [level 2] Title: 피보나치 수, Time: 28.28 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    var arr = [0, 1];
+    
+    for (let i = 2; i <= n; i++){
+        arr.push((arr[i-1]+arr[i-2]) % 1234567)
+    }
+        
+    return arr[n];
+}
