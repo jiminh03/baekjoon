8d565fb241cee19873ab7eaba20dd1773aea2b23
Jimin Hwang <101847439+jiminh03@users.noreply.github.com>
2026-02-19 12:25:44 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.82 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -3,10 +3,11 @@ function solution(A,B){
 
     A.sort((a,b) => a-b)
     B.sort((a,b) => b-a)
+    let sum = 0
     
     for (let i = 0; i < A.length; i++){
-        answer += A[i] * B[i]
+        sum += A[i] * B[i]    
     }
 
-    return answer;
+    return sum;
 }
