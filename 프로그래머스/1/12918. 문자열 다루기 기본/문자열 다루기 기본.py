ddc360b97b6f82b0115412a07e33d57e6a280cad
Jimin Hwang <101847439+jiminh03@users.noreply.github.com>
2026-03-19 00:06:37 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.00 ms, Memory: 9.07 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = True
+    
+    if len(s) != 4 and len(s) != 6:
+        return False
+    
+    for i in s:
+        if i.isalpha():
+            return False
+    
+    return answer
