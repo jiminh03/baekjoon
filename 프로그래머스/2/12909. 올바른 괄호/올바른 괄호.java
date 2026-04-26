23fcdb2ed82e6994c04337db6527271020c2b4d8
Jimin Hwang <101847439+jiminh03@users.noreply.github.com>
2026-04-26 12:57:03 +0900

    [level 2] Title: 올바른 괄호, Time: 17.27 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,26 +3,9 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        Deque<String> stack = new ArrayDeque<>();
+        String[] arr = s.split("");
         
-        for (int i = 0; i < s.length(); i++){
-            String text = s.substring(i,i+1);
-            
-            if (text.equals("(")){
-                stack.push(text);
-            }
-            else{
-                if (stack.isEmpty()){
-                    return false;
-                }
-                stack.pop();
-            }
-        }
-        
-        if (!stack.isEmpty()){
-            answer = false;
-        }
-        
+        System.out.println(Arrays.toString(arr));
 
         return answer;
     }
